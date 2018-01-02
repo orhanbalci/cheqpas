3e5c46b2bf764a95e6cdec2d74e690c350961416
EC2 Default User <orhanbalci@gmail.com>
2018-01-02 11:46:50 +0000

    cli added

--- src/main/scala/net/orhanbalci/cheqpas/Main.scala
+++ src/main/scala/net/orhanbalci/cheqpas/Main.scala
@@ -1,11 +1,24 @@
 package net.orhanbalci.cheqpas
 import atto._
 import Atto._
+import org.rogach.scallop._
+import better.files.{File => ScalaFile, _}
+import better.files.Dsl._
+import OverdraftChequeAnouncementParser._
 
-object Main extends Greeting with App {
-  println(greeting)
+class Conf(settings: Seq[String]) extends ScallopConf(settings) {
+  version("cheqpas 0.1.0 (c) 2017 Orhan Balci")
+  banner("""Usage: java -jar cheqpas.jar [OPTION]
+           |Overdraft Cheque Anouncemen File Parser
+           |Options:
+           |""".stripMargin)
+  val overdraftAnnouncementFile = opt[String](descr = "Overdraft Cheque Announcement File")
+  verify()
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+
+object Main extends App {
+    val conf = new Conf(args);
+    val overdraftFileContent = ScalaFile(conf.overdraftAnnouncementFile()).contentAsString;
+    overdraftChequeAnouncement.parse(overdraftFileContent)
 }
